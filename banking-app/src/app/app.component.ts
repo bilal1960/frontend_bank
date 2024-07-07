64879b08f257c8304f9ff905057e375aadc21925
Younes <bilal45001@hotmail.com>
2024-07-07 04:37:26 +0200

    implémentation interface my-transaction

--- banking-app/src/app/app.component.ts
+++ banking-app/src/app/app.component.ts
@@ -4,11 +4,12 @@ import {LoginComponent} from "./pages/login/login.component";
 import {RegisterComponent} from "./pages/register/register.component";
 import {MenuComponent} from "./components/menu/menu.component";
 import {UserDashboardComponent} from "./pages/user-dashboard/user-dashboard.component";
+import {MyTransactionsComponent} from "./pages/my-transactions/my-transactions.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, RegisterComponent, MenuComponent, UserDashboardComponent],
+  imports: [RouterOutlet, LoginComponent, RegisterComponent, MenuComponent, UserDashboardComponent, MyTransactionsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
