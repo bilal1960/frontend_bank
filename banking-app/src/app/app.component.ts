063a5b3f6d6734faf8cef6e92f54755eae13d29a
Younes <bilal45001@hotmail.com>
2024-07-07 02:35:59 +0200

    développement user-dashboard

--- banking-app/src/app/app.component.ts
+++ banking-app/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {LoginComponent} from "./pages/login/login.component";
 import {RegisterComponent} from "./pages/register/register.component";
 import {MenuComponent} from "./components/menu/menu.component";
+import {UserDashboardComponent} from "./pages/user-dashboard/user-dashboard.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, RegisterComponent, MenuComponent],
+  imports: [RouterOutlet, LoginComponent, RegisterComponent, MenuComponent, UserDashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
