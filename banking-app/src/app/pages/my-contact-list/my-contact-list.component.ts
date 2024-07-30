5cccac2f5e13ff4a11a486bfdcc324f1e3e127dd
Younes <bilal45001@hotmail.com>
2024-07-30 02:40:00 +0200

    implémentation pages my-contacts

--- banking-app/src/app/pages/my-contact-list/my-contact-list.component.ts
+++ banking-app/src/app/pages/my-contact-list/my-contact-list.component.ts
@@ -1,15 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {ContactService} from "../../services/services";
+import {HelperService} from "../../services/helper/helper.service";
+import {ContactDto} from "../../services/models/contact-dto";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-my-contact-list',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    NgForOf
   ],
   templateUrl: './my-contact-list.component.html',
   styleUrl: './my-contact-list.component.scss'
 })
-export class MyContactListComponent {
+export class MyContactListComponent implements OnInit{
+
+  contacts: Array<ContactDto> = [];
+  constructor(
+    private contactService: ContactService,
+    private helperService: HelperService
+  ) {
+  }
+  ngOnInit(): void {
+    this.contactService.findAllByUserId1({
+      "user-id": this.helperService.userId
+    }).subscribe({
+      next: (data) => {
+        this.contacts = data;
+      }
+    });
+  }
 
 }
