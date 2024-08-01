39b072ae6e004d8d99ce4133367148974184cda6
Younes <bilal45001@hotmail.com>
2024-08-01 23:28:48 +0200

    mise en place de la liaison new-contact

--- banking-app/src/app/pages/new-contact/new-contact.component.ts
+++ banking-app/src/app/pages/new-contact/new-contact.component.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ContactDto} from "../../services/models/contact-dto";
+import {ContactService} from "../../services/services/contact.service";
+import {HelperService} from "../../services/helper/helper.service";
+import {FormsModule} from "@angular/forms";
+import {ActivatedRoute, Router} from "@angular/router";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-new-contact',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    NgForOf,
+    NgIf
+  ],
   templateUrl: './new-contact.component.html',
   styleUrl: './new-contact.component.scss'
 })
-export class NewContactComponent {
+export class NewContactComponent implements OnInit{
 
+  contact: ContactDto = {};
+  errorMessages: Array<string> = [];
+
+  constructor(
+    private  contactService: ContactService,
+    private helperService: HelperService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
+  }
+  ngOnInit(): void {
+    const contactId = this.activatedRoute.snapshot.params["idContact"]
+    if(contactId) {
+      this.contactService.findById2({
+        "contact-id": this.activatedRoute.snapshot.params["idContact"]
+      }).subscribe({
+        next: (data) => {
+          this.contact = data;
+        }
+      });
+    }
+  }
+
+  save(){
+    this.errorMessages = [];
+    this.contact.userId = this.helperService.userId;
+    this.contactService.save2({
+      body: this.contact
+    }).subscribe({
+      next: async (data) => {
+        await this.router.navigate(["user/my-contact-list"])
+      },
+      error: (err) => {
+        this.errorMessages = err.error.validationErrors
+      }
+    })
+  }
+
+  async cancel() {
+    await this.router.navigate(["user/my-contact-list"])
+
+  }
 }
